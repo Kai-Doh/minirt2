cceead796925698af56eb06cba252de6057d5426
Kai-Doh <github@kaidoh.ch>
2026-02-18 16:22:21 +0100

    fix norminette

--- src/utils/parsing_utils.c
+++ src/utils/parsing_utils.c
@@ -38,18 +38,44 @@ t_bool	is_valid_double(char *str)
 	return (dot_count <= 1);
 }
 
+/* Parses fractional part of a double string */
+static double	parse_frac(char *str, int i)
+{
+	double	frac;
+	double	divisor;
+
+	frac = 0.0;
+	divisor = 10.0;
+	if (str[i] == '.')
+		i++;
+	while (str[i])
+	{
+		frac += (double)(str[i] - '0') / divisor;
+		divisor *= 10.0;
+		i++;
+	}
+	return (frac);
+}
+
 /* Parses string to double or returns -999999.0 */
 double	parse_double(char *str)
 {
 	double	result;
-	char	*endptr;
+	int		sign;
+	int		i;
 
 	if (!is_valid_double(str))
 		return (-999999.0);
-	result = strtod(str, &endptr);
-	if (*endptr != '\0')
-		return (-999999.0);
-	return (result);
+	result = 0.0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && str[i] != '.')
+		result = result * 10.0 + (double)(str[i++] - '0');
+	return (sign * (result + parse_frac(str, i)));
 }
 
 /* Frees a null-terminated array of strings */
